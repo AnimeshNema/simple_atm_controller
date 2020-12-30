22d5734f8855b9641ceb47b86ee8e329007f7878
AnimeshNema <34148425+AnimeshNema@users.noreply.github.com>
2020-12-30 00:00:51 -0500

    code clean up

--- AtmController.h
+++ AtmController.h
@@ -4,27 +4,6 @@
 #include<string>
 #include<map>
 
-//struct AccountInformation {
-//	int s_pin_number;
-//	int s_card_number;
-//	std::map<int,int> s_acc_and_balance;
-//};
-//
-//class Bank
-//{
-//private:
-//	std::vector<AccountInformation> m_data;
-//	
-//public:
-//	Bank();
-//	~Bank();
-//	std::vector<AccountInformation> addCustomer(int card_number, int pin_number, std::string account_type, int balance);
-//	std::vector<AccountInformation> addAccount(int card_number, std::string account_type, int balance);
-//	std::map<int, int> validatePIN(int card_number, int pin_number);
-//	void updateBalance(int card_number, int account_type, int amount);
-//	std::map<int, int> m_invalid_information;
-//};
-
 class Atm : public Bank
 {
 private:
